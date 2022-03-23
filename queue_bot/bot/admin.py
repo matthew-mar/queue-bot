5d91d2b2f02149e9e28c0d6810b83c2da26b5489
matthew-mar <mmary.prog@gmail.com>
2022-03-23 15:13:52 +0300

    admin intialization

--- queue_bot/bot/admin.py
+++ queue_bot/bot/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Chats, Admins, ChatMembers, Queues, QueuesChats
 
-# Register your models here.
+
+class ChatsAdmin(admin.ModelAdmin):
+    pass
+
+
+class AdminsAdmin(admin.ModelAdmin):
+    pass
+
+
+class ChatMembersAdmin(admin.ModelAdmin):
+    pass
+
+
+class QueuesAdmin(admin.ModelAdmin):
+    pass
+
+
+class QueuesChatsAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Chats, ChatsAdmin)
+admin.site.register(Admins, AdminsAdmin)
+admin.site.register(ChatMembers, ChatMembersAdmin)
+admin.site.register(Queues, QueuesAdmin)
+admin.site.register(QueuesChats, QueuesChatsAdmin)
