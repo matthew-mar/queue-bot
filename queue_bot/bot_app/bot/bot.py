c527fba8e79ae89b621d43dd95b87aa077129115
matthew-mar <mmary.prog@gmail.com>
2022-03-27 12:12:41 +0300

    echo bot

--- queue_bot/bot_app/bot/bot.py
+++ queue_bot/bot_app/bot/bot.py
@@ -4,12 +4,32 @@ from vk_api.utils import get_random_id
 
 
 def read_token() -> str:
+    """ функция считывает токен из файла """
     with open("./token.txt", "r") as token:
         token: str = token.read()
     return token
 
 
-# авторизация
-session = vk_api.VkApi(token=read_token())
-api = session.get_api()
-longpoll = VkLongPoll(session)
+def message_send(user_id: int, message: str) -> None:
+    """ 
+    функция реализует метод vk api messages.send 
+    описание метода: https://dev.vk.com/method/messages.send
+    """
+    api.messages.send(**{
+        "user_id": event.user_id,
+        "random_id": get_random_id(),
+        "message": event.text,
+    })
+
+
+if __name__ == "__main__":
+    # авторизация
+    session = vk_api.VkApi(token=read_token())  # создание новой сессии бота
+    api = session.get_api()  # получение доступа к методам vk api
+    longpoll = VkLongPoll(session)  # подключение к longpoll серверу
+
+    # "прослушивание" сервера
+    for event in longpoll.listen():
+        # эхо-бот
+        if event.to_me and event.type == VkEventType.MESSAGE_NEW:
+            message_send(user_id=event.user_id, message=event.text)
