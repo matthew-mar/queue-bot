58c84347fb032be88363b50dca5213dc1494bd26
matthew-mar <mmary.prog@gmail.com>
2022-03-28 13:22:29 +0300

    echo bot in chats

--- queue_bot/bot_app/bot/bot.py
+++ queue_bot/bot_app/bot/bot.py
@@ -1,35 +1,58 @@
-import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
+from requests import post
+import vk
 from vk_api.utils import get_random_id
+
+GROUP_ID: int = 206732640
+VK_API_VERSION: float = 5.131
 
 
 def read_token() -> str:
     """ функция считывает токен из файла """
-    with open("./token.txt", "r") as token:
-        token: str = token.read()
+    with open("token.txt", "r") as token_file:
+        token: str = token_file.read()
     return token
 
 
-def message_send(user_id: int, message: str) -> None:
+def messages_send(peer_id: int, message: str) -> None:
     """ 
-    функция реализует метод vk api messages.send 
-    описание метода: https://dev.vk.com/method/messages.send
+    отправка сообщения через метод vk api messages.send 
+    описание метода: https://vk.com/dev/messages.send
     """
     api.messages.send(**{
-        "user_id": event.user_id,
+        "peer_id": peer_id,
         "random_id": get_random_id(),
-        "message": event.text,
+        "message": message,
     })
 
 
 if __name__ == "__main__":
     # авторизация
-    session = vk_api.VkApi(token=read_token())  # создание новой сессии бота
-    api = session.get_api()  # получение доступа к методам vk api
-    longpoll = VkLongPoll(session)  # подключение к longpoll серверу
+    session: vk.Session = vk.Session(access_token=read_token())
+    api: vk.API = vk.API(session, v=VK_API_VERSION)
+    
+    # получение доступа к серверу
+    longpoll: dict = api.groups.getLongPollServer(group_id=GROUP_ID)
+    server, key, ts = longpoll["server"], longpoll["key"], longpoll["ts"]
+    
+    # прослушивание сервера
+    while True:
+        longpoll_response: dict = post(server, data={  # получение ответа
+            "act": "a_check",
+            "key": key,
+            "ts": ts,
+            "wait": 25
+        }).json()
 
-    # "прослушивание" сервера
-    for event in longpoll.listen():
-        # эхо-бот
-        if event.to_me and event.type == VkEventType.MESSAGE_NEW:
-            message_send(user_id=event.user_id, message=event.text)
+        # получение списка событий от сервера
+        longpoll_updates: list = longpoll_response["updates"]
+        if longpoll_updates:  # если обновления есть
+            # если получено новое сообщение
+            if longpoll_updates[0]["type"] == "message_new":
+                messages_send(  # отправка сообщения с текстом сообшения участника беседы
+                    peer_id=longpoll_updates[0]["object"]["message"]["peer_id"],
+                    message=longpoll_updates[0]["object"]["message"]["text"].lstrip("[club206732640|bot] "),
+                )
+
+        # изменение ts для следующего запроса
+        # ts - номер последнего события, начиная с которого нужно получать данные;
+        ts = longpoll_response["ts"]
