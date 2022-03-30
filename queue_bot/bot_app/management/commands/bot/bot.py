ad873cdd62a61d91fb725078acc3c77b331a181e
matthew-mar <mmary.prog@gmail.com>
2022-03-30 18:17:39 +0300

    добавление админа в базу

--- queue_bot/bot_app/management/commands/bot/bot.py
+++ queue_bot/bot_app/management/commands/bot/bot.py
@@ -1,11 +1,4 @@
-# from requests import post
-# import vk
-# from vk_api.utils import get_random_id
-# import sys
-
-# from django.conf import settings
-# import django
-
+from pprint import pprint
 from requests import post
 import vk
 from vk_api.utils import get_random_id
@@ -39,7 +32,18 @@ def messages_send(peer_id: int, message: str) -> None:
     })
 
 
-def main() -> None:
+def users_get(user_ids: int) -> dict:
+    """
+    получение данных о пользователе через метод vk api users.get
+    описание метода: https://dev.vk.com/method/users.get
+    """
+    users: list[dict] = api.users.get(**{
+        "user_ids": user_ids
+    })
+    return users[0]
+
+
+def start() -> None:
     # получение доступа к серверу
     longpoll: dict = api.groups.getLongPollServer(group_id=GROUP_ID)
     server, key, ts = longpoll["server"], longpoll["key"], longpoll["ts"]
@@ -56,12 +60,23 @@ def main() -> None:
         # получение списка событий от сервера
         longpoll_updates: list = longpoll_response["updates"]
         if longpoll_updates:  # если обновления есть
+            print(longpoll_updates)
             # если получено новое сообщение
-            if longpoll_updates[0]["type"] == "message_new":
-                if len(longpoll_updates[0]["object"]["message"]["text"]) == 0:
-                    if longpoll_updates[0]["object"]["message"]["from_id"]:
-                        admins = Member.objects.all().values_list("member_vk_id")
-                        print(admins)
+            if len(longpoll_updates[0]["object"]["message"]["text"]) == 0:
+                admins_vk_ids = [
+                    admin.member_vk_id 
+                    for admin in Member.objects.filter(is_admin=True)
+                ]
+                if longpoll_updates[0]["object"]["message"]["from_id"] not in admins_vk_ids:
+                    admin: dict = users_get(longpoll_updates[0]["object"]["message"]["from_id"])
+                    member: Member = Member(
+                        member_vk_id=admin["id"],
+                        is_admin=True,
+                        name=admin["first_name"],
+                        surname=admin["last_name"],
+                    )
+                    member.save()
+                    print("new admin")
 
         # изменение ts для следующего запроса
         # ts - номер последнего события, начиная с которого нужно получать данные;
@@ -71,66 +86,3 @@ def main() -> None:
 # авторизация
 session: vk.Session = vk.Session(access_token=read_token())
 api: vk.API = vk.API(session, v=VK_API_VERSION)
-
-
-# settings.configure()
-
-# django.setup()
-
-# from models import *
-
-# GROUP_ID: int = 206732640
-# VK_API_VERSION: float = 5.131
-
-
-# def read_token() -> str:
-#     """ функция считывает токен из файла """
-#     with open("bot/token.txt", "r") as token_file:
-#         token: str = token_file.read()
-#     return token
-
-
-# def messages_send(peer_id: int, message: str) -> None:
-#     """ 
-#     отправка сообщения через метод vk api messages.send 
-#     описание метода: https://vk.com/dev/messages.send
-#     """
-#     api.messages.send(**{
-#         "peer_id": peer_id,
-#         "random_id": get_random_id(),
-#         "message": message,
-#     })
-
-
-# if __name__ == "__main__":
-#     # авторизация
-#     session: vk.Session = vk.Session(access_token=read_token())
-#     api: vk.API = vk.API(session, v=VK_API_VERSION)
-    
-#     # получение доступа к серверу
-#     longpoll: dict = api.groups.getLongPollServer(group_id=GROUP_ID)
-#     server, key, ts = longpoll["server"], longpoll["key"], longpoll["ts"]
-    
-#     # прослушивание сервера
-#     while True:
-#         longpoll_response: dict = post(server, data={  # получение ответа
-#             "act": "a_check",
-#             "key": key,
-#             "ts": ts,
-#             "wait": 25
-#         }).json()
-
-#         # получение списка событий от сервера
-#         longpoll_updates: list = longpoll_response["updates"]
-#         if longpoll_updates:  # если обновления есть
-#             # если получено новое сообщение
-#             if longpoll_updates[0]["type"] == "message_new":
-#                 if len(longpoll_updates[0]["object"]["message"]["text"]) == 0:
-#                     if longpoll_updates[0]["message"]["from_id"]:
-#                         # admins = Member.objects.all().values_list("member_vk_id")
-#                         # print(admins)
-#                         pass
-
-#         # изменение ts для следующего запроса
-#         # ts - номер последнего события, начиная с которого нужно получать данные;
-#         ts = longpoll_response["ts"]
--- queue_bot/bot_app/management/commands/bot_command.py
+++ queue_bot/bot_app/management/commands/bot_command.py
@@ -1,20 +1,11 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 
-from django.conf import settings
-import django
-
-
-# settings.configure()
-
-django.setup()
-
-from ...models import Member
-from .bot.bot import main
+from .bot import bot
 
 class Command(BaseCommand):
     help = 'Displays current time'
 
     def handle(self, *args, **kwargs):
-        print(Member.objects.all())
-        main()
+        print("bot started")        
+        bot.start()
